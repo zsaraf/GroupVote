38136422374f6ff68988c439425df3f9cdb4f3ea
Raymond Kennedy <raymondk@stanford.edu>
2013-07-16 21:45:00 -0700

    Revert "Merge branch 'master' of https://github.com/zsaraf/GroupVote"
    This reverts commit 35a9ec5c3f60056c24c2a04266ee406819c976ad, reversing
    changes made to cc09f03a35b2e60a4d575276c4933c67f51793ed.

---
+++ Client/GroupVoteTests/GroupVoteTests.m
@@ -0,0 +1,32 @@
+//
+//  GroupVoteTests.m
+//  GroupVoteTests
+//
+//  Created by Alex Grover on 7/15/13.
+//  Copyright (c) 2013 Alex Grover. All rights reserved.
+//
+
+#import "GroupVoteTests.h"
+
+@implementation GroupVoteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GroupVoteTests");
+}
+
+@end
